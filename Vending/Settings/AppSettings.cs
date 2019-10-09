4f4657705d19e4f19e14a3d35e43d1b91b94199e
Sergey Novikov <s-n-o-v@yandex.ru>
2019-10-09 21:46:54 +0300

    Add files via upload

---
+++ Vending/Settings/AppSettings.cs
@@ -0,0 +1,40 @@
+﻿namespace Vending.Settings
+{
+    using System.Collections.Generic;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class AppSettings
+    {
+        public static int Cash
+        {
+            get
+            {
+                int cash = 0;
+                if (HttpContext.Current.Session["CASH"] != null)
+                {
+                    cash = (int)HttpContext.Current.Session["CASH"];
+                }
+
+                return cash;
+            }
+            set { HttpContext.Current.Session["CASH"] = value; }
+        }
+
+        public static string SecretKey
+        {
+            get
+            {
+                string key = null;
+                if (HttpContext.Current.Session["SKEY"] != null)
+                {
+                    key = (string)HttpContext.Current.Session["SKEY"];
+                }
+
+                return key;
+            }
+            set { HttpContext.Current.Session["SKEY"] = value; }
+        }
+
+    }
+}
